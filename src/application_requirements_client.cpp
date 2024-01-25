42c471b925b37fab51afccae9093a7bfdd145c40
ManuelAlbisu <malbi005@fiu.edu>
2024-01-25 15:33:40 -0500

    Implemented client.

--- src/application_requirements_client.cpp
+++ src/application_requirements_client.cpp
@@ -12,10 +12,17 @@ int main(int argc, char **argv) {
     rclcpp::Client<Requirements>::SharedPtr client = node->create_client<Requirements>("Requirements");
 
     auto request = std::make_shared<Requirements::Request>();
-    request->age = atol(argv[1]);
+    request->age = atoi(argv[1]);
     request->name = std::string(argv[2]);
-    request->is_citizen = bool(argv[3]);
-    request->is_student = bool(argv[4]);
+    request->is_citizen = std::string(argv[3]) == "true" || std::string(argv[3]) == "1" || std::string(argv[3]) == "yes";
+    request->is_student = std::string(argv[4]) == "true" || std::string(argv[4]) == "1" || std::string(argv[4]) == "yes";
+
+    auto result = client->async_send_request(request);
+    if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS)
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), 
+        "%s is %s for the position.\n", request->name.c_str(), result.get()->is_applicable ? "ELEGIBLE" : "NOT ELEGIBLE");
+    else 
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service requirements\n");
 
     rclcpp::shutdown();
 
--- src/application_requirements_server.cpp
+++ src/application_requirements_server.cpp
@@ -6,14 +6,16 @@
 using Requirements = application_requirements::srv::Requirements;
 
 void applicability(const std::shared_ptr<Requirements::Request> request, std::shared_ptr<Requirements::Response> response) {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Applicant information received.\nage: %d" "\nname: %s" "\ncitizen: %s" "\nstudent: %s",
+    response->is_applicable = false; 
+
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Applicant information received.\nage: %d \nname: %s \ncitizen: %s \nstudent: %s",
                 request->age, request->name.c_str(), request->is_citizen ? "yes" : "no", request->is_student ? "yes" : "no");
     
     if (request->age >= 18 && request->is_citizen && request->is_student)
         response->is_applicable = true;
 
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Applicant elegible?: %s", 
-                response->is_applicable ? "yes" : "no");
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Applicant elegibility: %s", 
+                response->is_applicable ? "elegible" : "not elegible");
 }
 
 int main(int argc, char **argv) {
